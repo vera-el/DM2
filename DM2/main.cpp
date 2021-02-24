b44b3080da8cb25733ed49162d67b49558d89452
ArtVeronic <ArtVeronic@gmail.com>
2021-02-24 18:31:05 +0100

    Initial Commit

---
+++ DM2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DM2
+//
+//  Created by Vera El Moutaouakil on 24.02.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
